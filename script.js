fa9a6ddf7cc86a9aa4515457f4ee0bbdb412cca7
Pignataro67 <Pignataro67@gmail.com>
2021-07-31 06:09:50 -0700

    variable assignment and Event Listener

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
+const ctx = canvas.getContext('2d');
+
 let size = 10
 let isPressed = false
 colorEl.value = 'black'
@@ -10,4 +19,11 @@ canvas.addEventListener('mousedown', (e) => {
 
   x = e.offsetX
   y = e.offsetY
+})
+
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false
+
+  x = undefined
+  y = undefined
 })
