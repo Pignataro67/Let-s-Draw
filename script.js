5807dec668987f4c517848a13881abd52600e03c
Pignataro67 <Pignataro67@gmail.com>
2021-08-04 04:48:03 -0700

    adding a function and event listeners

--- script.js
+++ script.js
@@ -70,3 +70,17 @@ increaseBtn.addEventListener('click', () => {
 
   updateSizeOnScreen()
 })
+
+decreaseBtn.addEventListener('click', () => {
+  size -= 5
+
+  if(size < 5) {
+      size = 5
+  }
+
+  updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
